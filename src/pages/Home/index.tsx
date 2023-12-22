b29c5b58c9f6a78eded19fa8772acef66b38f880
devandrewjpn <yuutoandrew.jpn@gmail.com>
2023-12-22 10:40:34 -0300

    upload

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,71 @@
+import { HandPalm, Play } from "phosphor-react";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormProvider, useForm } from "react-hook-form";
+
+import {
+  HomeContainer,
+  StartCountdownButton,
+  StopCountdownButton,
+} from "./styles";
+
+import { NewCycleForm } from "./components/NewCycleForm";
+import { Countdown } from "./components/Countdown";
+import { useContext } from "react";
+import { CyclesContext } from "../../contexts/CyclesContext";
+
+const newCycleFormValidationSChema = zod.object({
+  task: zod.string().min(1, "Informa a tarefa."),
+  minutesAmount: zod
+    .number()
+    .min(5, "O ciclo precisa ser de no mínimo 5 minutos.")
+    .max(60, "O ciclo precisa ser de no máximo 60 minutos"),
+});
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSChema>;
+
+export function Home() {
+  const { createNewCycle, interruptCurrentCycle, activeCycle } = useContext(CyclesContext)
+  const newCycleForm = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSChema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0,
+    },
+  });
+
+  const { handleSubmit, watch, reset } = newCycleForm;
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
+        <FormProvider {...newCycleForm}>
+          <NewCycleForm />
+        </FormProvider>
+        <Countdown />
+
+        {activeCycle ? (
+          <StopCountdownButton type="button" onClick={interruptCurrentCycle}>
+            <HandPalm size={24} />
+            Interromper
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
+      </form>
+    </HomeContainer>
+  );
+}
+export { CyclesContext };
+
---
+++ src/reducers/cycles/reducer.ts
@@ -0,0 +1,56 @@
+import { ActionTypes } from "./actions";
+import { produce } from 'immer'
+
+interface CyclesState {
+    cycles: Cycle[];
+    activeCycleId: string | null;
+  }
+
+ export interface Cycle {
+    id: string;
+    task: string;
+    minutesAmount: number;
+    startDate: Date;
+    interruptedDate?: Date;
+    finishedDate?: Date;
+  }
+
+
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function cyclesReducer(state: CyclesState, action: any) {
+
+    switch(action.type) {
+      case ActionTypes.ADD_NEW_CYCLE:
+        return produce(state, (draft) => {
+          draft.cycles.push(action.payload.newCycle)
+          draft.activeCycleId = action.payload.newCycle.id
+        })
+      case ActionTypes.INTERRUPT_CURRENT_CYCLE:
+        return {
+          ...state,
+          cycles: state.cycles.map((cycle) => {
+            if (cycle.id == state.activeCycleId) {
+              return { ...cycle, interruptedDate: new Date() };
+            } else {
+              return cycle;
+            }
+          }),
+          activeCycleId: null,
+        }
+      case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
+        return {
+          ...state,
+          cycles: state.cycles.map((cycle) => {
+            if (cycle.id == state.activeCycleId) {
+              return { ...cycle, finishedDate: new Date() };
+            } else {
+              return cycle;
+            }
+          }),
+          activeCycleId: null,
+        }
+      default :
+        return state
+    }
+  }
